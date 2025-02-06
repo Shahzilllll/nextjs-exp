3328d8afdb1d635eab2c21741af422dec5cecbc8
Shahzil <160623950+Shahzilllll@users.noreply.github.com>
2025-02-06 15:50:50 +0500

    Update dontmiss.tsx

--- app/components/dontmiss.tsx
+++ app/components/dontmiss.tsx
@@ -5,22 +5,21 @@ const Dontmiss = () => {
   return (
     <div className="bg-white min-h-screen flex flex-col items-center py-16 helvetica-neue">
       <div className="relative max-w-6xl w-full">
-        {/* Optimized Image */}
+      
         <Image
-          src="/img/dontmiss.png" // Use relative path from public/
+          src="/img/dontmiss.png" 
           alt="Flight Essentials"
           width={1440}
           height={700}
           className="shadow-lg"
         />
 
-        {/* Top Label */}
+
         <div className="absolute top-[-55px] left-[1px] bg-white bg-opacity-70 p-3 text-center font-bold text-lg text-black w-[140px] h-[27px] flex items-center justify-center helvetica-neue">
   Don't Miss
 </div>
       </div>
 
-      {/* Text Section */}
       <div className="text-center mt-8 px-4 w-full">
         <div className="mx-auto w-[806px]">
           <h1 className="text-black font-bold text-4xl helvetica-neue">FLIGHT ESSENTIALS</h1>
